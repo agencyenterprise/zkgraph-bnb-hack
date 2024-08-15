f9954b4d773985058b0fe4a36e9c7a88c9483c1b
kevinkons <kevin.kons@hotmail.com>
2024-08-14 22:28:47 -0300

    Adding inference call

--- web/src/app/api/inference/route.ts
+++ web/src/app/api/inference/route.ts
@@ -2,12 +2,26 @@ import { getLogged } from "@/utils/login";
 
 export async function POST(req: Request) {
   const data = await req.json();
-
-  console.log("payload", data);
-
   const session = await getLogged();
 
-  console.log("session", session);
+  const backendUrl = process.env.BACKEND_URL || "http://localhost:8000";
+  const backendAuthToken = process.env.BACKEND_AUTH_TOKEN || "";
+  const response = await fetch(`${backendUrl}/proof_requests`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Auth-Token": backendAuthToken,
+    },
+    body: JSON.stringify({
+      name: "pr3",
+      description: "pr3",
+      graph_url: "pr3",
+      ai_model_name: "",
+      ai_model_inputs: ""
+    })
+  })
+
+  console.log({response})
 
   return Response.json({
     session,
