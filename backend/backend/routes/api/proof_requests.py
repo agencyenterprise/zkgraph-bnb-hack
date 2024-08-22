6703977328efbb7b032f3a74807045dcdab73b00
kevinkons <kevin.kons@hotmail.com>
2024-08-22 14:18:08 -0300

    getting address from api token

--- backend/backend/routes/api/proof_requests.py
+++ backend/backend/routes/api/proof_requests.py
@@ -8,10 +8,9 @@ router = APIRouter()
 
 @router.get("/{owner_wallet}")
 async def get_proof_requests(
-    owner_wallet: str = Path(..., alias="owner_wallet"),
-    _ = Depends(authenticate_w_api_token),
+    user = Depends(authenticate_w_api_token),
 ):
-    proofs = proof_requests_collection.find({"owner_wallet": owner_wallet })
+    proofs = proof_requests_collection.find({"owner_wallet": user.address })
     return list_serial(proofs)
 
 @router.post("/")
