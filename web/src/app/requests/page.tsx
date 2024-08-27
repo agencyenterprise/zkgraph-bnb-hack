a4e6370e8a8def1cf5cd6a2861698d5ab230437b
Andre Borges Medeiros <andborges@gmail.com>
2024-08-27 16:46:29 -0300

    Update link display and behavior

--- web/src/app/requests/page.tsx
+++ web/src/app/requests/page.tsx
@@ -84,9 +84,13 @@ export default function Me() {
                   </p>
                   {proofRequest.worker_wallet != "None" && <p className="text-left text-sm text-secondary-200">
                     Paid to worker:
-                    <a href={`https://testnet.bscscan.com/address/${proofRequest.worker_wallet}`} target='_blank'>{' '}
-                      ${proofRequest.worker_wallet.slice(0, 6)}...{proofRequest.worker_wallet.slice(-4)}
-                    </a>
+                    <Link
+                      href={`https://testnet.bscscan.com/address/${proofRequest.worker_wallet}`}
+                      target="_blank"
+                      className="underline text-primary-500 hover:text-primary-400"
+                    >
+                      {proofRequest.worker_wallet.slice(0, 6)}...{proofRequest.worker_wallet.slice(-4)}
+                    </Link>
                   </p>}
                 </div>
               ))}
