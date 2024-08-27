e649be7fc0b6481b3051e633eafb76bed5756c6e
kevinkons <kevin.kons@hotmail.com>
2024-08-26 21:23:51 -0300

    making route call proof request service

--- backend/backend/routes/proof_requests.py
+++ backend/backend/routes/proof_requests.py
@@ -1,13 +1,7 @@
-import json
-import os
 from fastapi import APIRouter, Body, Depends, Path
 from backend.auth.auth import authenticate
 from backend.config.database import proof_requests_collection
-from backend.schemas.proof_request_schema import list_serial, individual_serial
-from backend.blockchain.transaction import encode_transaction_data, wait_for_confirmations
-from backend.blockchain.defender import RelayerClient
-from pymongo.errors import PyMongoError
-from backend.services.queue_service import PikaClient
+from backend.schemas.proof_request_schema import list_serial
 from backend.services.proof_requests_service import create_proof_request
 
 router = APIRouter()
@@ -29,50 +23,4 @@ async def create_proof_request_endpoint(
     ai_model_inputs: str = Body(),
     _ = Depends(authenticate),
 ):
-    try:
-        proofRequest = ProofRequest(
-            owner_wallet=owner_wallet,
-            name=name,
-            description=description,
-            ai_model_name=ai_model_name,
-            ai_model_inputs=ai_model_inputs
-        )
-
-        result = proof_requests_collection.insert_one(proofRequest.dict())
-        proofRequest._id = str(result.inserted_id)
-
-        relayer = RelayerClient(os.getenv("DEFENDER_API_KEY"), os.getenv("DEFENDER_API_SECRET"))
-        tx = relayer.send_transaction({
-            "to": os.getenv("ESCROW_ADDRESS"),
-            "data": encode_transaction_data("lock(address,uint256)", ["address", "uint256"], [owner_wallet, 1000000000000000]),
-            "gasLimit": 1000000,
-        })
-
-        receipt = wait_for_confirmations(tx["hash"])
-        
-        if receipt["status"] == 0:
-            raise Exception(f"Transaction failed: {tx['hash']}")
-    except PyMongoError as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Database insertion failed: {str(e)}"
-        )
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Relayer transaction failed: {str(e)}"
-        )
-
-    # Send proof request to the queue
-    proof_request_message = {
-        "proof_request_id": proofRequest._id,
-        "ai_model_name": ai_model_name,
-        "ai_model_inputs": ai_model_inputs,
-    }
-
-    proof_request_message_body = json.dumps(proof_request_message)
- 
-    pika_client = PikaClient(os.environ.get("RABBITMQ_URL"))
-    await pika_client.publish("requests_queue", proof_request_message_body)
-
-    return individual_serial(proofRequest.dict())
+    create_proof_request(owner_wallet, name, description, ai_model_name, ai_model_inputs)
--- backend/backend/services/proof_requests_service.py
+++ backend/backend/services/proof_requests_service.py
@@ -4,7 +4,7 @@ from fastapi import HTTPException, status
 from backend.models.proof_request import ProofRequest
 from backend.config.database import proof_requests_collection
 from backend.schemas.proof_request_schema import individual_serial
-from backend.blockchain.transaction import encode_transaction_data
+from backend.blockchain.transaction import encode_transaction_data, wait_for_confirmations
 from backend.blockchain.defender import RelayerClient
 from pymongo.errors import PyMongoError
 from backend.services.queue_service import PikaClient
@@ -30,11 +30,16 @@ async def create_proof_request(
         proofRequest._id = str(result.inserted_id)
 
         relayer = RelayerClient(os.getenv("DEFENDER_API_KEY"), os.getenv("DEFENDER_API_SECRET"))
-        relayer.send_transaction({
+        tx = relayer.send_transaction({
             "to": os.getenv("ESCROW_ADDRESS"),
             "data": encode_transaction_data("lock(address,uint256)", ["address", "uint256"], [owner_wallet, 1000000000000000]),
             "gasLimit": 1000000,
         })
+
+        receipt = wait_for_confirmations(tx["hash"])
+        
+        if receipt["status"] == 0:
+            raise Exception(f"Transaction failed: {tx['hash']}")
     except PyMongoError as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -46,6 +51,7 @@ async def create_proof_request(
             detail=f"Relayer transaction failed: {str(e)}"
         )
 
+    # Send proof request to the queue
     proof_request_message = {
         "proof_request_id": proofRequest._id,
         "ai_model_name": ai_model_name,
