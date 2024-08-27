d1b3e065c19e412bcf9eaa778bfa4ae28adfb9a4
Andre Borges Medeiros <andborges@gmail.com>
2024-08-27 15:33:13 -0300

    Add "status" field to proof_request API

--- backend/backend/schemas/proof_request_schema.py
+++ backend/backend/schemas/proof_request_schema.py
@@ -5,6 +5,7 @@ def individual_serial(proof_request) -> dict:
         "description": str(proof_request["description"]),
         "ai_model_name": str(proof_request["ai_model_name"]),
         "ai_model_inputs": str(proof_request["ai_model_inputs"]),
+        "status": str(proof_request["status"]),
     }
 
 def list_serial(proof_requests) -> list:
