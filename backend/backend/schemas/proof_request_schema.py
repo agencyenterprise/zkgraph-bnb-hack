73cebc664cdad3437c83be09a493b407dd4f19c9
Andre Borges Medeiros <andborges@gmail.com>
2024-08-28 10:58:26 -0300

    Add proof field to proof request page

--- backend/backend/schemas/proof_request_schema.py
+++ backend/backend/schemas/proof_request_schema.py
@@ -1,12 +1,13 @@
 def individual_serial(proof_request) -> dict:
     return {
-        # "id": proof_request["_id"],
+        "id": str(proof_request["_id"]),
         "name": str(proof_request["name"]),
         "description": str(proof_request["description"]),
         "ai_model_name": str(proof_request["ai_model_name"]),
         "ai_model_inputs": str(proof_request["ai_model_inputs"]),
         "status": str(proof_request["status"]),
         "worker_wallet": str(proof_request["worker_wallet"]),
+        "proof": str(proof_request["proof"]),
     }
 
 def list_serial(proof_requests) -> list:
